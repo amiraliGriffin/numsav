683ec522b534a3cffb29a61564c36c4bd8edb5c0
amirali <amir.boeing797@gmail.com>
2024-01-09 01:12:11 +0330

    main update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ bot = Client(
     name = "Pa",
     api_id = 29585063,
     api_hash = "7cbc271846d81539d10bdec6cf36ba6a",
-    bot_token = "6881221790:AAH-p3mQYh0Z3vIwigZuiwQ7om1sAe5iKP0",
+    bot_token = "6703791627:AAEcP1rPAloQmS5Gx-AIHDsTxo8-H5MFktU",
     plugins = plug,
 )
 
